9fd378c5ad640cf741dfbd4039315700687de13c
robertredbox <robert@redboxmobile.com>
2025-03-12 15:00:26 +0000

    Update main page to use the new AppTabs component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Dashboard from "@/components/dashboard/dashboard";
+import { AppTabs } from "@/components/app-tabs";
 
 export default function Home() {
   return (
-    <main>
-      <Dashboard />
+    <main className="min-h-screen p-8">
+      <h1 className="text-3xl mb-8" style={{ fontFamily: '"Roboto Slab", serif', fontWeight: 500 }}>
+        William Hill Sports - ASO Dashboard
+      </h1>
+      <AppTabs />
     </main>
   );
 }
